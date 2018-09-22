9f7943bae6e01a05ed0e03382ae35c41d84ca0e1
Stephen Medeiros <scmphdpe@gmail.com>
2018-09-22 10:54:09 -0400

    still working on CAFNN

--- NNcuratedActivationFunction.py
+++ NNcuratedActivationFunction.py
@@ -25,7 +25,7 @@ def x2f(x, deriv=True):
     return x**2
 
 # input data
-n_data = 4
+n_data = 1000
 x = np.array(np.random.rand(n_data))
 
 y = np.array(x**2)
@@ -40,32 +40,26 @@ syn2 = np.random.random((4,1))
 syn3 = np.random.random((4,1))
 
 # training
-for j in range(10):
+for j in range(n_data):
     # layers
-    l0 = x # input layer
-    l1in = np.dot(l0, syn0)
-    print(l1in)
-#     # curated function layer
-#     l1 = np.array([sigmoid(l1in[0]),
-#                    sinef(l1in[1]),
-#                    cosf(l1in[2]),
-#                    x2f(l1in[3])])
-#     # output layer
-#     l2 = np.dot(l1, syn1)
-#
-#     #backpropagation
-#     l2_error = y - l2
-#
-#     if (j % 10000) == 0:
-#         print('Error:' + str(np.mean(np.abs(l2_error))))
-#
-#     l2_delta = l2_error*nonlin(l2, deriv=True)
-#     l1_error = l2_delta.dot(syn1.T)
-#     l1_delta = l1_error*nonlin(l1, deriv=True)
-#
-#     # update synapses
-#     syn1 += l1.T.dot(l2_delta)
-#     syn0 += l0.T.dot(l1_delta)
-#
-# print('output after training')
-# print(l2)
+    l0 = x[j] # input
+    l1in = np.array([sigmoid(l0),
+                   sinef(l0),
+                   cosf(l0),
+                   x2f(l0)])
+    l1 = np.dot(l1in, syn0)
+    # output layer
+    l2 = np.dot(l1, syn1)
+
+    #backpropagation
+    l2_error = y[j] - l2
+    l2_delta = np.array(l2_error*nonlin(l2, deriv=True)
+    l1_error = l2_delta.dot(syn1.T)
+    l1_delta = l1_error*nonlin(l1, deriv=True)
+
+    # update synapses
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += l0.T.dot(l1_delta)
+
+print('output after training')
+print(l2)
